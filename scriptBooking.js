a86e83a3200ea0dc967be54a0c25527169437ba3
LauriePlisson <plisson.laurie@gmail.com>
2025-07-02 15:58:27 +0200

    manque fetch booking

--- scriptBooking.js
+++ scriptBooking.js
@@ -15,17 +15,46 @@ function creaBooking() {
           <p id="enjoy">Enjoy your travels with TicketHack!</p>`;
 }
 
+function changeDate(date) {
+  let hour = moment(date).format("HH:MM");
+  console.log(hour);
+  return hour;
+}
 //crea ticket train
 function ajbooking(tab) {
   creaBooking();
-  for (let obj of tab)
+
+  for (let obj of tab) {
+    let date = Date.now();
+    // console.log(date);
+    let heure = changeDate(date);
+    // console.log(heure);
+    let time = obj.hour - heure;
     document.querySelector(".bookingContenair").innerHTML += `
             <div class="displayBooking">
               <div class="textBooking">
                 <p class="trajet">${obj.departure}>${obj.arrival}</p>
-                <p class="heure">${obj.date}</p>
+                <p class="heure">${obj.hour}</p>
                 <p class="prix">${obj.price}€</p>
-                <p class="tempsDepart">Departure in time</p>
+                <p class="tempsDepart">Departure in ${time}</p>
               </div>
             </div>`;
+  }
 }
+
+let test = [
+  {
+    departure: "Paris",
+    arrival: "Lyon",
+    hour: "18:45",
+    price: "110",
+  },
+  {
+    departure: "Paris",
+    arrival: "Lyon",
+    hour: "19:45",
+    price: "110",
+  },
+];
+
+ajbooking(test);
